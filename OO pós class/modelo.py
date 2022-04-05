760a685ad78b2507c81934f83e359782fe91ba61
VLTTECH <luizmaias@hotmail.com>
2022-04-04 23:50:55 -0300

    segundo commit da pasta

--- OO pós class/modelo.py
+++ OO pós class/modelo.py
@@ -32,6 +32,21 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
       
+class Playlist():
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
 
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+`
 
 
